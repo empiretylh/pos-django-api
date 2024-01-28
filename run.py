0d43ffe017cbda669685e43b3de018a1ef43d8fe
= <=>
2024-01-28 15:37:38 +0630

    Update DJANGO_SETTINGS_MODULE in run.py

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ import sys
 from django.core.management import execute_from_command_line
 
 if __name__ == "__main__":
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'your_project.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
 
     # Read the domain from the file
     with open('domain.txt', 'r') as file:
